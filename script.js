6f192d22932a95e6a52af448ebcc7d4be7127471
Natalia Davydova <davydovanatalia063@gmail.com>
2021-06-10 19:13:57 -0700

    add getDate function, add getChanges, modify renderCheapDay, modify renderCheapYear

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ const formSearch = document.querySelector('.form-search'),
   dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'),
   inputCitiesTo = document.querySelector('.input__cities-to'),
   dropdownCitiesTo = document.querySelector('.dropdown__cities-to'),
-  inputDateDepart = document.querySelector('.input__date-depart');
+  inputDateDepart = document.querySelector('.input__date-depart'),
+  cheapestTicket = document.getElementById('cheapest-ticket'),
+  otherCheapTickets = document.getElementById('other-cheap-tickets');
 
 const citiesApi = 'dataBase/cities.json',
   proxy = 'https://cors-anywhere.herokuapp.com/corsdemo',
@@ -54,11 +56,77 @@ const selectCity = (e, input, list) => {
   }
 };
 
+const getNameCity = (code) => {
+  const objCity = city.find((item) => item.code === code);
+  return objCity.name;
+};
+
+const getDate = (date) => {
+  return new Date(date).toLocaleString('ru', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit'
+  });
+};
+
+const getChanges = (num) => {
+  if (num) {
+    return num === 1 ? 'With one stop' : 'with two stops';
+  } else {
+    return 'Without changing flights'
+  }
+};
+
+const createCard = (data) => {
+  const ticket = document.createElement('article');
+  ticket.classList.add('ticket');
+
+  let deep = '';
+  if (data) {
+    deep = `
+    <h3 class="agent">${data.gate}</h3>
+    <div class="ticket__wrapper">
+      <div class="left-side">
+        <a href="https://www.aviasales.ru" class="button button__buy">Buy for ${data.value}rub</a>
+      </div>
+      <div class="right-side">
+          <div class="block-left">
+             <div class="city__from">Departure from
+               <span class="city__name">${getNameCity(data.origin)}</span>
+             </div>
+            <div class="date">${getDate(data.depart_date)}</div>
+          </div>
+
+          <div class="block-right">
+           <div class="changes">${getChanges(data.number_of_changes)}</div>
+           <div class="city__to">Arrival city:
+             <span class="city__name">${getNameCity(data.destination)}</span>
+            </div>
+          </div>
+      </div>
+    </div>
+    `;
+  } else {
+    deep = '<h3>Unfortunately there are no tickets</h3>'
+  }
+
+  ticket.insertAdjacentHTML('afterbegin', deep);
+  return ticket;
+};
+
 const renderCheapDay = (cheapTicket) => {
-  console.log(cheapTicket)
-}
+  cheapestTicket.style.display = 'block';
+  cheapestTicket.innerHTML = `<h2>The cheapest ticket for this date</h2>`;
+
+  const ticket = createCard(cheapTicket[0]);
+  cheapestTicket.append(ticket)
+};
 
 const renderCheapYear = (cheapTickets) => {
+  otherCheapTickets.style.display = 'block';
+  otherCheapTickets.innerHTML = `<h2>The cheapest ticket for another dates</h2>`;
   cheapTickets.sort((a, b) => a.value - b.value);
   console.log(cheapTickets)
 };
@@ -91,22 +159,28 @@ dropdownCitiesTo.addEventListener('click', e => {
 formSearch.addEventListener('submit', (e) => {
   e.preventDefault();
 
+
   const cityFrom = city.find((item) => inputCitiesFrom.value === item.name);
   const cityTo = city.find((item) => inputCitiesTo.value === item.name);
 
   const formData = {
-    from: cityFrom.code,
-    to: cityTo.code,
+    from: cityFrom,
+    to: cityTo,
     when: inputDateDepart.value,
   }
-  const requestData = '?depart_date=' + formData.when +
-    '&origin=' + formData.from +
-    '&destination=' + formData.to +
-    '&one_way=true';
+  if (formData.from && formData.to) {
+    const requestData = '?depart_date=' + formData.when +
+      '&origin=' + formData.from.code +
+      '&destination=' + formData.to.code +
+      '&one_way=true';
 
-  getData(calendar + requestData, (response) => {
-    renderCheap(response, formData.when);
-  })
+    getData(calendar + requestData, (response) => {
+      renderCheap(response, formData.when);
+    });
+  } else {
+    console.log('enter the correct city name')
+  }
+
 });
 
 getData(citiesApi, (data) => {
